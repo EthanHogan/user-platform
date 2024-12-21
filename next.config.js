88e2fdf579213dcbccfaa1802ed05173fad0cf71
EthanHogan <ethan.hogan7@gmail.com>
2024-12-21 13:50:51 -0600

    switch from pages router to app router

--- next.config.js
+++ next.config.js
@@ -6,23 +6,12 @@ import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
 const config = {
-  reactStrictMode: true,
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
-  /**
-   * If you are using `appDir` then you must comment the below `i18n` config out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
-  transpilePackages: ["geist"],
 };
 
 export default config;
