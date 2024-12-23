32ba39a9307a6f92baff940d294217ff5d8850b5
EthanHogan <ethan.hogan7@gmail.com>
2024-12-22 23:01:25 -0600

    styles posts page with table component

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,18 +1,30 @@
 import { Suspense } from "react";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "~/components/ui/table";
 import { db } from "~/server/db";
 
 export default function PostsPage() {
   return (
     <div className="border border-green-500 p-1">
       <h2 className="text-xl">Posts Page</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Id</th>
-            <th>Name</th>
-          </tr>
-        </thead>
-        <tbody>
+      <Table>
+        <TableCaption>A list of your recent invoices.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Invoice</TableHead>
+            <TableHead>Status</TableHead>
+            <TableHead>Method</TableHead>
+            <TableHead className="text-right">Amount</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
           <Suspense
             fallback={
               <tr>
@@ -22,8 +34,8 @@ export default function PostsPage() {
           >
             <Posts />
           </Suspense>
-        </tbody>
-      </table>
+        </TableBody>
+      </Table>
     </div>
   );
 }
@@ -34,14 +46,10 @@ const Posts = async () => {
   // wait 3 seconds
   await new Promise((resolve) => setTimeout(resolve, 3000));
 
-  return (
-    <>
-      {posts.map((post) => (
-        <tr key={post.id}>
-          <td>{post.id}</td>
-          <td>{post.name}</td>
-        </tr>
-      ))}
-    </>
-  );
+  return posts.map((post) => (
+    <TableRow key={post.id}>
+      <TableCell className="font-medium">{post.id}</TableCell>
+      <TableCell>{post.name}</TableCell>
+    </TableRow>
+  ));
 };
